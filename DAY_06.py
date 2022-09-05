1eecb224ffb0fd5a2663bae1d42358a2141b594d
PANKAJ SEN <73360623+Senpankaj@users.noreply.github.com>
2022-09-05 06:26:03 +0530

    Update DAY_06.py

--- DAY_06.py
+++ DAY_06.py
@@ -22,3 +22,22 @@ while number_of_hurdles > 0:
     print(number_of_hurdles)
 
 #Hurdle 2 :
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left() 
+    
+while not at_goal():
+    jump()
+    
